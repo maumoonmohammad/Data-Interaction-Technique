2760ea0276e2ed12fcb30916f63248310adf80ad
maumoonmohammad <maumoon.sheikh@gmail.com>
2023-11-28 20:49:03 -0500

    first change

--- src/index.js
+++ src/index.js
@@ -2,12 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import LineChart from './line';
+import Barchart from './barchart';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        {/* <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route> */}
+        <Route path="/" element={<App />} />
+        <Route path="/line" element={<LineChart />} />
+        <Route path="/barchart" element={<Barchart />} />
+
+
+
+
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
